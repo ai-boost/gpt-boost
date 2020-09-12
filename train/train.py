248cb75c8e84fc121ec40976cec617bb4f74e7c5
ai-boost <asq2020@88.com>
2020-09-13 02:57:08 +0800

    edit gitignore; edit train.py

--- train/train.py
+++ train/train.py
@@ -1,52 +1,48 @@
-from transformers import DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
-from transformers import GPT2TokenizerFast
-from transformers import GPT2LMHeadModel
-from transformers import TextDataset
+from transformers import TextDataset, DataCollatorForLanguageModeling
+from transformers import GPT2LMHeadModel, GPT2Config, GPT2TokenizerFast
 
 
 def run():
-    model_path = "E:\\data\\models\\gpt-boost\\gpt-2-layer-memory"
-    data_path = "E:\\data\\corpus\\gpt-boost\\novel.txt"
-    tokenizer_path = ".\\tokenizer"
-    tb_path = ".\\runs\\gpt-2-layer-memory"
+    tb_path = ".\\runs\\novel-gpt-tiny"
+    data_path = "E:\\data\\corpus\\gpt-boost\\novel_1G.txt"
+    tokenizer_path = "E:\\data\\models\\tokenizer"
+    model_path = "E:\\data\\models\\gpt-boost\\novel-gpt-tiny"
 
-    n_embd = 512
     n_head = 8
-    n_layer = 2
+    n_layer = 1
+    n_embd = 512
     n_positions = 256
     vocab_size = 20000
-    finetuning_mode = False
 
+    warmup_steps = 5000
     learning_rate = 1e-4
-    gradient_accumulation_steps = 1
     num_train_epochs = 10
-    per_device_train_batch_size = 1
+    finetuning_mode = False
+    gradient_accumulation_steps = 4
+    per_device_train_batch_size = 48
 
-    tokenizer = GPT2TokenizerFast.from_pretrained(tokenizer_path, max_len=256)
+    print("initializing tokenizer and model...")
 
+    tokenizer = GPT2TokenizerFast.from_pretrained(tokenizer_path, max_len=n_positions)
     if finetuning_mode:
         model = GPT2LMHeadModel.from_pretrained(model_path)
     else:
-        from transformers import GPT2Config
-
         config = GPT2Config(
             n_embd=n_embd,
             n_head=n_head,
             n_layer=n_layer,
+            vocab_size=vocab_size,
             n_positions=n_positions,
-            vocab_size=vocab_size
         )
         model = GPT2LMHeadModel(config=config)
 
-    # %%
-
-    model.num_parameters()
+    print("model parameters: ", model.num_parameters(), "\nloading datasets...")
 
     dataset = TextDataset(
+        block_size=256,
         tokenizer=tokenizer,
         file_path=data_path,
-        block_size=256,
     )
 
     data_collator = DataCollatorForLanguageModeling(
@@ -54,28 +50,26 @@ def run():
     )
 
     training_args = TrainingArguments(
-        output_dir=model_path,
-        overwrite_output_dir=True,
-        no_cuda=False,
-        do_train=True,
         fp16=True,
+        do_train=True,
+        save_total_limit=5,
         logging_dir=tb_path,
-        logging_steps=100,
+        output_dir=model_path,
+        prediction_loss_only=True,
+        overwrite_output_dir=True,
+        dataloader_drop_last=True,
+        warmup_steps=warmup_steps,
         learning_rate=learning_rate,
-        gradient_accumulation_steps=gradient_accumulation_steps,
         num_train_epochs=num_train_epochs,
-        per_device_train_batch_size=per_device_train_batch_size,
-        warmup_steps=10000,
-        save_steps=1000,
-        save_total_limit=2
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        per_device_train_batch_size=per_device_train_batch_size
     )
 
     trainer = Trainer(
         model=model,
         args=training_args,
-        data_collator=data_collator,
         train_dataset=dataset,
-        prediction_loss_only=True,
+        data_collator=data_collator
     )
 
     trainer.train()
