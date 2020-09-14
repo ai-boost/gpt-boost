69da5558d147ed8e9622285c82d3cb9512ecf967
ai-boost <asq2020@88.com>
2020-09-14 23:51:50 +0800

    add argparse; add model config file;

--- train/train.py
+++ train/train.py
@@ -1,3 +1,5 @@
+import argparse
+
 from transformers import Trainer, TrainingArguments
 from transformers import DataCollatorForLanguageModeling
 from transformers import GPT2LMHeadModel, GPT2Config, GPT2TokenizerFast
@@ -5,46 +7,44 @@ from transformers import GPT2LMHeadModel, GPT2Config, GPT2TokenizerFast
 from utils.data_loader import TextDataset
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, help="model path")
+    parser.add_argument("--tb_path", type=str, help="tensorboard log path")
+    parser.add_argument("--tokenizer_path", type=str, help="tokenizer path")
+    parser.add_argument("--model_config_path", type=str, help="config path")
+    parser.add_argument("--data_path", type=str, help="training dataset path")
+    parser.add_argument("--warmup_steps", default=10000, type=int, help="warm up steps")
+    parser.add_argument("--learning_rate", default=1e-4, type=float, help="learning rate")
+    parser.add_argument("--num_train_epochs", default=10, type=int, help="training epochs")
+    parser.add_argument("--save_total_limit", default=3, type=int, help="save total limit")
+    parser.add_argument("--finetuning_mode", default=False, type=bool, help="fine tune or pretrain")
+    parser.add_argument("--gradient_accumulation_steps", type=int, help="gradient accumulation steps")
+    parser.add_argument("--per_device_train_batch_size", type=int, help="training batch size per device")
+    args = parser.parse_args()
+
+    return args
+
+
 def run():
-    tb_path = ".\\runs\\novel-gpt-tiny"
-    data_path = "E:\\data\\corpus\\gpt-boost\\all.txt"
-    tokenizer_path = "E:\\data\\models\\tokenizer"
-    model_path = "E:\\data\\models\\gpt-boost\\novel-gpt-tiny"
-
-    n_head = 8
-    n_layer = 1
-    n_embd = 512
-    n_positions = 256
-    vocab_size = 20000
-
-    warmup_steps = 10000
-    learning_rate = 1e-4
-    num_train_epochs = 10
-    finetuning_mode = False
-    gradient_accumulation_steps = 4
-    per_device_train_batch_size = 48
+    args = get_args()
 
     print("initializing tokenizer and model...")
 
-    tokenizer = GPT2TokenizerFast.from_pretrained(tokenizer_path, max_len=n_positions)
-    if finetuning_mode:
-        model = GPT2LMHeadModel.from_pretrained(model_path)
+    tokenizer = GPT2TokenizerFast.from_pretrained(args.tokenizer_path)
+    if args.finetuning_mode:
+        config = GPT2Config.from_pretrained(args.model_path)
+        model = GPT2LMHeadModel.from_pretrained(args.model_path)
     else:
-        config = GPT2Config(
-            n_embd=n_embd,
-            n_head=n_head,
-            n_layer=n_layer,
-            vocab_size=vocab_size,
-            n_positions=n_positions,
-        )
+        config = GPT2Config.from_pretrained(args.model_config_path)
         model = GPT2LMHeadModel(config=config)
 
     print("model parameters: ", model.num_parameters(), "\nloading datasets...")
 
     dataset = TextDataset(
-        block_size=256,
+        block_size=config.n_positions,
         tokenizer=tokenizer,
-        file_path=data_path,
+        file_path=args.data_path,
     )
 
     data_collator = DataCollatorForLanguageModeling(
@@ -54,17 +54,17 @@ def run():
     training_args = TrainingArguments(
         fp16=True,
         do_train=True,
-        save_total_limit=5,
-        logging_dir=tb_path,
-        output_dir=model_path,
+        logging_dir=args.tb_path,
         prediction_loss_only=True,
         overwrite_output_dir=True,
         dataloader_drop_last=True,
-        warmup_steps=warmup_steps,
-        learning_rate=learning_rate,
-        num_train_epochs=num_train_epochs,
-        gradient_accumulation_steps=gradient_accumulation_steps,
-        per_device_train_batch_size=per_device_train_batch_size
+        output_dir=args.model_path,
+        warmup_steps=args.warmup_steps,
+        learning_rate=args.learning_rate,
+        save_total_limit=args.save_total_limit,
+        num_train_epochs=args.num_train_epochs,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        per_device_train_batch_size=args.per_device_train_batch_size
     )
 
     trainer = Trainer(
@@ -75,7 +75,7 @@ def run():
     )
 
     trainer.train()
-    trainer.save_model(model_path)
+    trainer.save_model(args.model_path)
 
 
 if __name__ == '__main__':
